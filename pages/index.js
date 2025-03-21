f75191723df9ae6e55902099a44303452b04b447
Luís Miguel <108488402+LuisMiguelADS@users.noreply.github.com>
2025-03-21 03:40:11 +0000

    Modificando o texto novamente

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home() {
     var text = "O Brasil vence nas eliminatórias no estádio Mané Garrincha";
-    return <div><h1>Brasil 2 X 1 Colômbia</h1> <p>var</p></div>
+    return <div><h1>Brasil 2 X 1 Colômbia</h1> <p>${text}</p></div>
 }
 
 export default Home;
