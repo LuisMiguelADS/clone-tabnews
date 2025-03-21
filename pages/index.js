8bafc53aa4eea1cc195e0fd5bb1e00f5a897273b
Luís Miguel <108488402+LuisMiguelADS@users.noreply.github.com>
2025-03-21 22:41:26 +0000

    Consertando texto do

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
 function Home() {
-  var text = "O Brasil vence nas eliminatórias no estádio Mané Garrincha";
   return (
     <div>
-      <h1>Brasil 2 X 1 Colômbia</h1> <p>${text}</p>
+      <h1>Brasil 2 X 1 Colômbia</h1>{" "}
+      <p>O Brasil vence nas eliminatórias no estádio Mané Garrincha</p>
     </div>
   );
 }
-
 export default Home;
