b36a472e27b813c83ada9d44a4bf89418489b9c6
Luís Miguel <108488402+LuisMiguelADS@users.noreply.github.com>
2025-03-21 17:34:40 +0000

    Adicoina scripts `link:check` e `link:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 function Home() {
-    var text = "O Brasil vence nas eliminatórias no estádio Mané Garrincha";
-    return <div><h1>Brasil 2 X 1 Colômbia</h1> <p>${text}</p></div>
+  var text = "O Brasil vence nas eliminatórias no estádio Mané Garrincha";
+  return (
+    <div>
+      <h1>Brasil 2 X 1 Colômbia</h1> <p>${text}</p>
+    </div>
+  );
 }
 
 export default Home;
