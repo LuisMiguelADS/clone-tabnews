2061aec4f638e95dc6f4adc90a643dd226d6d453
Luís Miguel <108488402+LuisMiguelADS@users.noreply.github.com>
2025-03-22 01:56:33 +0000

    Alterando texto no `index.js`

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 function Home() {
   return (
     <div>
-      <h1>Brasil 2 X 1 Colômbia</h1>{" "}
-      <p>O Brasil vence nas eliminatórias no estádio Mané Garrincha</p>
+      <h1>Sarah, eu amo você</h1>
     </div>
   );
 }
